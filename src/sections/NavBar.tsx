83ff1954b716da5c73aa5cca01ac934c3f41785d
Brandon Perfetti <brandon@brandonperfetti.com>
2025-07-01 08:17:50 -0700

    Add Hero, NavBar, and ComingSoon components; implement useMaskSettings for responsive design

---
+++ src/sections/NavBar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+	return (
+		<nav>
+			<img src="/images/nav-logo.svg" alt="Navigation Logo" className="scale-90" />
+			<img src="/images/menu.svg" alt="Menu Icon" className="w-10" />
+		</nav>
+	);
+};
+
+export default NavBar;
